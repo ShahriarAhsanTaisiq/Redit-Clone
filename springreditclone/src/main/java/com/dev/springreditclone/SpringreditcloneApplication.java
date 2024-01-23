b72f6d3aa5a50ea5284bf393f8560569090790ce
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-01-23 13:33:04 +0600

    Enable Async annotation

--- springreditclone/src/main/java/com/dev/springreditclone/SpringreditcloneApplication.java
+++ springreditclone/src/main/java/com/dev/springreditclone/SpringreditcloneApplication.java
@@ -2,8 +2,10 @@ package com.dev.springreditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class SpringreditcloneApplication {
 
 	public static void main(String[] args) {
