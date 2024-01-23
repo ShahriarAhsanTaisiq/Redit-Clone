94941a99af6d4798b4ead274a23566f5ede28d6e
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-01-23 13:33:55 +0600

    Add findByUserName method

--- springreditclone/src/main/java/com/dev/springreditclone/repository/UserRepository.java
+++ springreditclone/src/main/java/com/dev/springreditclone/repository/UserRepository.java
@@ -5,6 +5,10 @@ import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository  extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
+
