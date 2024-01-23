65afd0e43b623587136e180740e383035762cfd0
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-01-23 12:40:29 +0600

    Implements verifyaccount and fetchUserandEnable method from AuthService

--- springreditclone/src/main/java/com/dev/springreditclone/service/AuthServiceImpl.java
+++ springreditclone/src/main/java/com/dev/springreditclone/service/AuthServiceImpl.java
@@ -1,17 +1,18 @@
 package com.dev.springreditclone.service;
 
 import com.dev.springreditclone.dto.RegisterRequest;
+import com.dev.springreditclone.exception.SpringReditException;
+import com.dev.springreditclone.model.NotificationEmail;
 import com.dev.springreditclone.model.User;
 import com.dev.springreditclone.model.VerificationToken;
 import com.dev.springreditclone.repository.UserRepository;
 import com.dev.springreditclone.repository.VerificationTokenRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.Instant;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -20,6 +21,7 @@ public class AuthServiceImpl implements AuthService{
     private final   PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final VerificationTokenRepository verificationTokenRepository;
+    private final MailServiceImpl mailService;
 
     @Override
     @Transactional
@@ -33,6 +35,26 @@ public class AuthServiceImpl implements AuthService{
         userRepository.save(user);
         
         String token  = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please activate your account",
+                user.getEmail(),"Thank you for signing up to Spring Reddit, " +
+                "please click on the below url to activate your account : " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
+    }
+
+    @Override
+    public void verifyAccount(String token) {
+      Optional<VerificationToken> verificationToken= verificationTokenRepository.findByToken(token);
+      verificationToken.orElseThrow(()->new SpringReditException("Invalid Token"));
+      fetchUseAndEnable(verificationToken.get());
+    }
+
+    @Transactional
+    public void fetchUseAndEnable(VerificationToken verificationToken) {
+       String username= verificationToken.getUser().getUsername();
+       User user = userRepository.findByUsername(username).orElseThrow(()->
+               new SpringReditException("User not found with name " + username));
+       user.setEnabled(true);
+       userRepository.save(user);
     }
 
     private String generateVerificationToken(User user) {
