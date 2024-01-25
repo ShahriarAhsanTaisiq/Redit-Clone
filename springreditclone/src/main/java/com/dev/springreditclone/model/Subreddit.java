e60c809c24d05465e520adab209314f9f7163dac
ShahriarAhsanTaisiq <shahriarahsantaisiq@gmail.com>
2024-01-25 17:34:05 +0600

    Update Subreddit entity and dto class.

--- springreditclone/src/main/java/com/dev/springreditclone/model/Subreddit.java
+++ springreditclone/src/main/java/com/dev/springreditclone/model/Subreddit.java
@@ -3,7 +3,6 @@ package com.dev.springreditclone.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.Instant;
 import java.util.List;
@@ -12,19 +11,22 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Subredit {
+public class Subreddit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotBlank(message = "Community name is required")
     private String name;
-    @NotBlank(message = "Decsciption is required")
-    private String descriotion;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
     @OneToMany(fetch = FetchType.LAZY)
     private List<Post> posts;
+
     private Instant createdDate;
+
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
-
-
 }
